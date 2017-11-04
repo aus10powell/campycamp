c53ea979d7c839eeea6c1b14a975410a5f718f68
blaan <blaan@protonmail.com>
2017-11-03 22:26:43 -0700

    update url

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
             <input type="submit" value="submit"/>
           </form>
           <Map 
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo9b_gwCyRnjm3kCoaLlOBAB0S52SAPkQ&v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
